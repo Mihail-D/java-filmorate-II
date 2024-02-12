7217324b13572fff405137bf2579e0539856a0d3
Nuby <nuby.4register@gmail.com>
2024-02-12 11:25:22 +0300

    FilmControllerImplTest

--- src/main/java/ru/yandex/practicum/filmorate/utility/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/utility/FilmValidator.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.utility;
 
 import lombok.experimental.UtilityClass;
-import ru.yandex.practicum.filmorate.exceptions.InputDataErrorException;
-import ru.yandex.practicum.filmorate.exceptions.InvalidDateException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
