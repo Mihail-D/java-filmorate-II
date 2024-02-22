69bfb622028a6ef244bcbddd4fe5e53e1260b457
Nuby <nuby.4register@gmail.com>
2024-02-22 15:20:02 +0300

    ErrorResponse

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
@@ -2,14 +2,15 @@ package ru.yandex.practicum.filmorate.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotExistException.class)
-    public ResponseEntity<String> handleUserNotFoundException(UserNotExistException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleUserNotExistException(UserNotExistException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
