6caa8d8bbbc21cfb44f6df16935120759a05a259
Nuby <nuby.4register@gmail.com>
2024-02-23 19:50:51 +0300

    GlobalExceptionHandler improvement

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
@@ -13,4 +13,36 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(FilmAlreadyExistException.class)
+    public ResponseEntity<ErrorResponse> handleFilmAlreadyExistException(FilmAlreadyExistException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(FilmNotExistException.class)
+    public ResponseEntity<ErrorResponse> handleFilmNotExistException(FilmNotExistException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InputDataErrorException.class)
+    public ResponseEntity<ErrorResponse> handleInputDataErrorException(InputDataErrorException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidDateException(InvalidDateException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 }
+
+
