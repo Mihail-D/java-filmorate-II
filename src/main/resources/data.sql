0d88c3f6bb2724bedcc9842b732fbd6ff3869354
Nuby <nuby.4register@gmail.com>
2024-03-05 11:02:50 +0300

    User section clean up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO mpa (mpa_id, name, description)
+VALUES (1, 'G', 'General audiences');
+
+INSERT INTO mpa (mpa_id, name, description)
+VALUES (2, 'PG', 'Parental guidance suggested');
+
+INSERT INTO mpa (mpa_id, name, description)
+VALUES (3, 'PG-13', 'Parents strongly cautioned');
+
+INSERT INTO mpa (mpa_id, name, description)
+VALUES (4, 'R', 'Restricted');
+
+INSERT INTO mpa (mpa_id, name, description)
+VALUES (5, 'NC-17', 'Adults Only');
+
+INSERT INTO genre (genre_id, name)
+VALUES (1, 'Комедия');
+
+INSERT INTO genre (genre_id, name)
+VALUES (2, 'Драма');
+
+INSERT INTO genre (genre_id, name)
+VALUES (3, 'Мультфильм');
+
+INSERT INTO genre (genre_id, name)
+VALUES (4, 'Триллер');
+
+INSERT INTO genre (genre_id, name)
+VALUES (5, 'Документальный');
+
+INSERT INTO genre (genre_id, name)
+VALUES (6, 'Боевик');
