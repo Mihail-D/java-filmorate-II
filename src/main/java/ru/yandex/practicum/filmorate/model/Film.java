9a37945a103f792ba2df88901913fa72e5f53b98
Nuby <nuby.4register@gmail.com>
2024-03-06 21:15:23 +0300

    Genre field refactoring

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -27,51 +27,9 @@ public class Film {
     @Positive
     private int duration;
 
-    private int genreId;
+    private Genre genre;
 
     private Mpa mpa;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
 
-        Film film = (Film) o;
-
-        if (getId() != film.getId()) {
-            return false;
-        }
-        if (getDuration() != film.getDuration()) {
-            return false;
-        }
-        if (getGenreId() != film.getGenreId()) {
-            return false;
-        }
-        if (!getName().equals(film.getName())) {
-            return false;
-        }
-        if (getDescription() != null ? !getDescription().equals(film.getDescription()) : film.getDescription() != null) {
-            return false;
-        }
-        if (!getReleaseDate().equals(film.getReleaseDate())) {
-            return false;
-        }
-        return getMpa().equals(film.getMpa());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (getId() ^ (getId() >>> 32));
-        result = 31 * result + getName().hashCode();
-        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
-        result = 31 * result + getReleaseDate().hashCode();
-        result = 31 * result + getDuration();
-        result = 31 * result + getGenreId();
-        result = 31 * result + getMpa().hashCode();
-        return result;
-    }
 }
